3ab5000fb7632ef5907cc982ea1035714f4b68c6
Alexander Bushukin <46033931+Protivsporta@users.noreply.github.com>
2022-03-17 23:23:02 +0300

    Delete

--- tasks/mint.js
+++
@@ -1,8 +0,0 @@
-task("mint", "Create amount of tokens on the address")
-  .addParam("account", "Address to mint")
-  .addParam("value", "Amount of tokens to mint")
-  .setAction(async (taskArgs) => {
-    const erc20 = await hre.ethers.getContractAt("ERC20", process.env.CONTRACT_ADDR);
-    await erc20.mint(taskArgs.account, taskArgs.value);
-    console.log(`${taskArgs.value} tokens minted to ${taskArgs.account}!`);
-  });
