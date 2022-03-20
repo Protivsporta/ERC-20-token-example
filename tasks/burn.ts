3dce480f1406a3a1c585d404f8da1c5d3cfa21dd
Alexander Bushukin <46033931+Protivsporta@users.noreply.github.com>
2022-03-20 11:55:03 +0300

    Added tasks for burn and mint

---
+++ tasks/burn.ts
@@ -0,0 +1,11 @@
+import { task } from 'hardhat/config';
+import '@nomiclabs/hardhat-ethers';
+
+task("burn", "Burn amount of tokens on the address")
+  .addParam("account", "Address to mint")
+  .addParam("value", "Amount of tokens to mint")
+  .setAction(async (taskArgs, hre) => {
+    const erc20 = await hre.ethers.getContractAt("ERC20", process.env.CONTRACT_ADDR!);
+    await erc20.burn(taskArgs.account, taskArgs.value);
+    console.log(`${taskArgs.value} tokens minted to ${taskArgs.account}!`);
+  });
